0b0c5c9f660a76976a131a3db468886bc5009ae5
aistephen <ifeoluwa.adebayo@decagon.dev>
2022-04-16 14:42:50 +0100

    test refactored

--- src/test/java/com/iteesoft/storeapp/StoreApplicationTests.java
+++ src/test/java/com/iteesoft/storeapp/StoreApplicationTests.java
@@ -3,11 +3,8 @@ package com.iteesoft.storeapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StoreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
